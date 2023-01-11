7c1f4423d576f2e7adaeb29c5ed7fab3a7a194a0
MatanRaf <matanraf@gmail.com>
2023-01-11 15:19:45 +0200

    created points for astroid

--- algorithmics/enemy/radar.py
+++ algorithmics/enemy/radar.py
@@ -1,5 +1,16 @@
+import math
+from typing import List
+
+import numpy as np
+
 from algorithmics.enemy.enemy import Enemy
 from algorithmics.utils.coordinate import Coordinate
+
+
+def get_bounding_circle(center, radius, point_count):
+    angles = [math.radians(theta) for theta in range(0, 360, 360//point_count)]
+    return [Coordinate(center.x + radius * math.cos(theta), center.y + radius * math.sin(theta)) for
+            theta in angles]
 
 
 class Radar(Enemy):
@@ -12,3 +23,12 @@ class Radar(Enemy):
         """
         self.center = center
         self.radius = radius
+
+    def get_points(self) -> List[Coordinate]:
+        points = []
+        min_radius = 3
+        jump = 3
+        for r in np.arange(min_radius, int(self.radius + jump), jump):
+            points += get_bounding_circle(self.center, r, r)
+
+        return points
