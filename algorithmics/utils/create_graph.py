a99dbc8b839e6eeb31036f52b3a9fbfec3d22642
TLP-274 <iftach.farkash.44@gmail.com>
2023-01-11 10:31:55 +0200

    .

--- algorithmics/utils/create_graph.py
+++ algorithmics/utils/create_graph.py
@@ -8,9 +8,11 @@ from algorithmics.utils.coordinate import Coordinate
 
 def init_graph(start: Coordinate, end: Coordinate, enemies: List[Enemy]):
     main_graph = nx.Graph()
-    main_graph.add_edge(start)
-    main_graph.add_edge(end)
-    main_graph.add_edge(start, end, dist=start.distance_to(end))
+    main_graph.add_node(start)
+    main_graph.add_node(end)
     for enemy in enemies:
         main_graph.add_nodes_from(enemy.get_points())
+        for coord in enemy.get_points():
+            main_graph.add_edge(start, coord, dist=start.distance_to(coord))
+            main_graph.add_edge(coord, end, dist=coord.distance_to(end))
     return main_graph
